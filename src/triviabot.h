26a4a72e54d2b1eec2d7cb658ec3a8731d392112
Steven Williams <saw12f@my.fsu.edu>
2016-11-29 14:13:39 -0500

    TriviaBot
    Added the main game class that will import questions, manage scores, and
    provide questions, answers, and hints.

---
+++ src/triviabot.h
@@ -0,0 +1,31 @@
+#ifndef _TRIVIABOT_H_
+#define _TRIVIABOT_H_
+
+#include <map>
+#include <fstream>
+#include <string>
+#include "stringutils.h"
+
+class TriviaBot {
+public:
+	TriviaBot();
+
+	// question bank
+	void ImportQuestions(const char* path, bool overwrite = false);
+
+	// player interactions
+	void AddPlayer    (const int& sd);
+	void RemovePlayer (const int& sd);
+
+	// game functions
+	std::string GetRandomQuestion ()              const;
+	std::string GetAnswer         (const std::string& question) const;
+	std::string GetHint           (const std::string& question) const;
+	void        IncreaseScore     (const int& sd);
+
+private:
+	std::map<int, int>                 player_scores_;
+	std::map<std::string, std::string> questions_;
+};
+
+#endif
